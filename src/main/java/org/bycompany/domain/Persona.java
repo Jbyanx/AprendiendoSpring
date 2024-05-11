31a78ad505a81105ffd7f46701826bd602bc5d46
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2024-05-11 02:02:34 -0500

    Compartiendo datos a la vista con Thymeleaf

--- src/main/java/org/bycompany/domain/Persona.java
+++ src/main/java/org/bycompany/domain/Persona.java
@@ -1,4 +1,11 @@
 package org.bycompany.domain;
 
+import lombok.Data;
+
+@Data
 public class Persona {
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String telefono;
 }
