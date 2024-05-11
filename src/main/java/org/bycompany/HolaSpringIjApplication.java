0620cd2ebb09588011ae54dd8b4fdd2eaa49cf48
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2024-05-11 00:10:49 -0500

    Hola mundo

---
+++ src/main/java/org/bycompany/HolaSpringIjApplication.java
@@ -0,0 +1,13 @@
+package org.bycompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolaSpringIjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HolaSpringIjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/bycompany/HolaSpringIjApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bycompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolaSpringIjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
