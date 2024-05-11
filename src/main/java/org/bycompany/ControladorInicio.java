0c25d02e3b6cc5507c58a72bb41ce1bd1fef9eff
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2024-05-11 01:00:04 -0500

    Hola mundo Thymeleaf

--- src/main/java/org/bycompany/ControladorInicio.java
+++ src/main/java/org/bycompany/ControladorInicio.java
@@ -1,17 +1,25 @@
 package org.bycompany;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @Slf4j
 public class ControladorInicio {
-
+    @Value("${index.saludo}")
+    private String saludo;
     @GetMapping("/")
-    public String inicio(){
-        log.info("ejecutando el controlador rest");
-        return "hola mundo con spring boot";
+    public String inicio(Model model){
+        var saludar = "pasando objetos con thymeleaf";
+
+        model.addAttribute("mensaje",saludar);
+        model.addAttribute("saludo",saludo);
+        log.info("ejecutando el controlador spring MVC");
+
+        return "index";
     }
 
 }
---
+++ src/main/java/org/bycompany/domain/Persona.java
@@ -0,0 +1,4 @@
+package org.bycompany.domain;
+
+public class Persona {
+}
