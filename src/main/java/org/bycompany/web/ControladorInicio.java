bd90a99e562b18b6c23e997c02761f203a8eeb9c
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2024-05-11 20:57:03 -0500

    CRUD funcionando

--- src/main/java/org/bycompany/web/ControladorInicio.java
+++ src/main/java/org/bycompany/web/ControladorInicio.java
@@ -1,11 +1,13 @@
 package org.bycompany.web;
 
 import lombok.extern.slf4j.Slf4j;
+import org.bycompany.domain.Persona;
 import org.bycompany.service.PersonaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @Slf4j
@@ -23,4 +25,27 @@ public class ControladorInicio {
         return "index";
     }
 
+    @GetMapping("/agregar")
+    public String agregar(Persona persona){
+        return "formulario";
+    }
+
+    @PostMapping("/guardar")
+    public String guardar(Persona persona){
+        personaService.guardar(persona);
+        return "redirect:/";
+    }
+
+    @GetMapping("/editar/{id}")
+    public String editar(Persona persona, Model model){
+        persona = personaService.encontrarPersona(persona);
+        model.addAttribute("persona", persona);
+        return "formulario";
+    }
+
+    @GetMapping("/eliminar/{id}")
+    public String eliminar(Persona persona){
+        personaService.eliminar(persona);
+        return "redirect:/";
+    }
 }
